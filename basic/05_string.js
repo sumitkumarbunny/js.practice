99b9d6969b6da1b12f6ac3fd0f98f16ef9d15389
Sumit Kumar <120630589+sumitkumarbunny@users.noreply.github.com>
2024-08-28 17:57:00 +0000

    string

---
+++ basic/05_string.js
@@ -0,0 +1,19 @@
+const name = "sumit"
+const repoCount = 10
+
+// console.log(name +repoCount + "value");
+
+console.log('helo my name is ${name} and my repo count is ${repocount}')
+
+const gameName = new String('sumit.kumar')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.charAt('3'));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
