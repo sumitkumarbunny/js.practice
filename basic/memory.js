4e7bda48e205a373c8c7707c5c7bf8d1990c294a
Sumit Kumar <120630589+sumitkumarbunny@users.noreply.github.com>
2024-08-28 17:38:14 +0000

    memory

---
+++ basic/memory.js
@@ -0,0 +1,21 @@
+// stack memory(primitive),heap memory(non primitive)
+
+let myName="sumit"
+
+// let anotherName = rahul
+
+anotherName="suman"
+
+console.log(anotherName);
+
+console.log(myName);
+
+let userOne = {
+    email: "sk@gmail.com",
+    upi:"sk@ybl"
+}
+let usertwo=userOne
+
+console.log(userOne.email);
+console.log(usertwo.email);
+
