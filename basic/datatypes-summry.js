79afd6700028e0250e047b480474f484b1601e25
Sumit Kumar <120630589+sumitkumarbunny@users.noreply.github.com>
2024-08-28 17:26:22 +0000

    datatype-summry

---
+++ basic/datatypes-summry.js
@@ -0,0 +1,33 @@
+// primitive
+
+// 7 types: string,number,boolean,null,,undefined,symbol,BigInt
+
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const OutsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id=== anotherId);
+const BigNumber = 123456789n
+
+// refrance type(non primitive)
+
+// array, objects,functions
+
+const heros =["shaktiman","nagraj","doga"]
+let myObj ={
+    name: "sumit",
+    age:25,
+}
+
+const myFunction = function(){
+    console.log("hello");
+}
+
+console.log(typeof anotherId);
